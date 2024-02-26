bc0b2ebf2c3c6f2169dc3546b387f329a07051e6
sonpham28052002 <sonpham28052002@gmail.com>
2024-02-26 19:34:00 +0700

    fix merge component contacts

--- src/components/chat/chatbox/NavChat.js
+++ src/components/chat/chatbox/NavChat.js
@@ -1,42 +1,10 @@
 import React from "react";
-import { HiOutlineUserAdd } from "react-icons/hi";
-import { AiOutlineUsergroupAdd } from "react-icons/ai";
-import { IoIosSearch } from "react-icons/io";
+import HeaderNavChat from "./headerNavChat";
 
 export default function NavChat() {
   return (
     <div className="h-full  w-1/5  border-r">
-      <div className="h-20 flex flex-col justify-between border-b  p-3">
-        <div className="flex flex-row justify-evenly items-center">
-          <div className="w-5/6 relative">
-            <input
-              type="text"
-              placeholder="Tìm kiếm"
-              maxLength={0}
-              readOnly
-              className=" w-full  h-8 border p-1 text-xs rounded pl-7 bg-[#eaedf0] focus:outline-none"
-              onClick={() => {
-                console.log("con");
-              }}
-            />
-            <IoIosSearch className="absolute top-1.5 left-1 text-xl text-slate-400" />
-          </div>
-          <div className="flex flex-row justify-evenly items-center ">
-            <button className=" h-7 w-7 mx-1 px-1 hover:bg-[#eaedf0] rounded">
-              <HiOutlineUserAdd className="text-xl " />
-            </button>
-            <button className=" h-7 w-7 px-1 hover:bg-[#eaedf0] rounded">
-              <AiOutlineUsergroupAdd className="text-xl " />
-            </button>
-          </div>
-        </div>
-        <div className="bg-red-400 flex flex-row justify-between items-end">
-          <div>
-            
-          </div>
-          <div>b</div>
-        </div>
-      </div>
+      <HeaderNavChat/>
     </div>
   );
 }
--- src/components/chat/contacts/index.js
+++ src/components/chat/contacts/index.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import { useState } from "react";
+import { IoPersonAddOutline } from "react-icons/io5";
+import ViewListFriend from "./ViewListFriend";
+import ViewListGroup from "./ViewListGroup";
+import ViewFriendRequests from "./ViewFriendRequests";
+import HeaderNavChat from "../chatbox/headerNavChat";
 
-export default function Contact(props) {
-    
+function Contacts() {
+  const [active, setActive] = useState(1);
 
-    return (
-        <div className='w-full h-full'>
-            contact
+  const handleButtonClick = (buttonId) => {
+    setActive(buttonId);
+  };
+
+  return (
+    <div className="flex flex-row h-screen ">
+      <div className="w-96 border-r-2 border-gray-200">
+        <HeaderNavChat />
+        <div className="flex-1  ">
+          <button
+            className="flex flex-row h-16 w-full items-center"
+            onClick={() => handleButtonClick(1)}
+            style={{ backgroundColor: active === 1 ? "#e5efff" : "white" }}
+          >
+            <div className="w-16">
+              <IoPersonAddOutline className="w-7 h-7 mx-auto" />
+            </div>
+            <div className="font-semibold">Danh sách bạn bè</div>
+          </button>
+
+          <button
+            className="flex flex-row h-16 w-full items-center"
+            onClick={() => handleButtonClick(2)}
+            style={{ backgroundColor: active === 2 ? "#e5efff" : "white" }}
+          >
+            <div className="w-16">
+              <IoPersonAddOutline className="w-7 h-7 mx-auto" />
+            </div>
+            <div className="font-semibold">Danh sách nhóm</div>
+          </button>
+
+          <button
+            className="flex flex-row h-16 w-full items-center"
+            onClick={() => handleButtonClick(3)}
+            style={{ backgroundColor: active === 3 ? "#e5efff" : "white" }}
+          >
+            <div className="w-16">
+              <IoPersonAddOutline className="w-7 h-7 mx-auto" />
+            </div>
+            <div className="font-semibold">Lời mời kết bạn</div>
+          </button>
         </div>
-    )
+      </div>
+
+      {active === 1 && <ViewListFriend />}
+      {active === 2 && <ViewListGroup />}
+      {active === 3 && <ViewFriendRequests />}
+    </div>
+  );
 }
+
+export default Contacts;
