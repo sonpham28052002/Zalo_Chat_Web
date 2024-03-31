e5381675c513da23fd711a4185a82e038cd1c4a1
sonpham28052002 <sonpham28052002@gmail.com>
2024-03-31 22:55:01 +0700

    handle fix timeout runtime

--- src/firebase/firebaseService.js
+++ src/firebase/firebaseService.js
@@ -1,21 +1,21 @@
 import { FIREBASE_AUTH } from "./firebaseConfig";
 import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
+const recreateRecaptchaVerifier = async () => {
+  window.recaptchaVerifier = new RecaptchaVerifier(FIREBASE_AUTH, "recaptcha", {
+    size: "invisible",
+  });
+  await window.recaptchaVerifier.render().then((widgetId) => {
+    window.recaptchaWidgetId = widgetId;
+  });
+};
+
 var handleVertifi = async (inputPhone) => {
   try {
     if (!window.recaptchaVerifier) {
-      window.recaptchaVerifier = new RecaptchaVerifier(
-        FIREBASE_AUTH,
-        "recaptcha",
-        {
-          size: "invisible",
-        }
-      );
-      await window.recaptchaVerifier.render().then((widgetId) => {
-        window.recaptchaWidgetId = widgetId;
-      });
+      await recreateRecaptchaVerifier();
     } else {
-      window.grecaptcha.reset(window.recaptchaWidgetId);
+      await window.grecaptcha.reset(window.recaptchaWidgetId);
     }
     return await signInWithPhoneNumber(
       FIREBASE_AUTH,
@@ -23,10 +23,20 @@ var handleVertifi = async (inputPhone) => {
       window.recaptchaVerifier
     );
   } catch (error) {
+    console.log(error);
     if (error.code === "auth/too-many-requests") {
       alert("Quá nhiều yêu cầu. Vui lòng thử lại sau.");
     } else if (error.message.includes("auth/invalid-phone-number")) {
       alert("Số điện thoại không hợp lệ. Vui lòng kiểm tra lại.");
+    } else if (
+      error.message.includes("reCAPTCHA client element has been removed")
+    ) {
+      await recreateRecaptchaVerifier();
+      return await signInWithPhoneNumber(
+        FIREBASE_AUTH,
+        inputPhone,
+        window.recaptchaVerifier
+      );
     }
   }
 };
